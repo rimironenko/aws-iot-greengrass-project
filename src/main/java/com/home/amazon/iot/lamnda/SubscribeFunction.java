cdc462cbd266df30c59ea271d2deb7c170dcbabf
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-09-16 17:34:55 +0300

    Added adding of the JSON messages to SNS

--- src/main/java/com/home/amazon/iot/lamnda/SubscribeFunction.java
+++ src/main/java/com/home/amazon/iot/lamnda/SubscribeFunction.java
@@ -5,6 +5,11 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.home.amazon.iot.model.Item;
+import software.amazon.awssdk.core.SdkSystemSetting;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,6 +20,17 @@ public class SubscribeFunction implements RequestHandler<Map<String, String>, St
 
     private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
     private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+    private final SqsClient sqsClient;
+    private final String queueUrl;
+
+    public SubscribeFunction() {
+        sqsClient = SqsClient.builder()
+                .region(Region.of(System.getenv(SdkSystemSetting.AWS_REGION.environmentVariable())))
+                .httpClientBuilder(UrlConnectionHttpClient.builder())
+                .build();
+        queueUrl = System.getenv("QueueUrl");
+    }
 
     @Override
     public String handleRequest(Map<String, String> input, Context context) {
@@ -27,11 +43,21 @@ public class SubscribeFunction implements RequestHandler<Map<String, String>, St
                 Item item = YAML_MAPPER.readValue(tempFile.toFile(), Item.class);
                 String json = JSON_MAPPER.writeValueAsString(item);
                 System.out.println("Converted to JSON: " + json);
+                sendSqsMessage(json);
             } catch (IOException e) {
                 System.out.println("Failed to save message: " + e);
             }
         }
         return "Subscribe function is invoked";
+    }
+
+    private void sendSqsMessage(String json) {
+        SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(json)
+                .delaySeconds(5)
+                .build();
+        sqsClient.sendMessage(sendMsgRequest);
     }
 
 }
