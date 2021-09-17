c057ef55ab304c0214f1a1664fde8a3745efba08
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-09-17 12:01:31 +0300

    Added user data script to provision an EC2 instance as Greengrass device

--- scripts/sendMessage.sh
+++ scripts/sendMessage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 sendMessage.py \
         --endpoint ENDPOINT \
-        --rootCA AmazonRootCA1.pem \
+        --rootCA root.ca.pem \
         --cert HASH.cert.pem \
         --key HASH.private.key \
         --thingName THING_NAME \
