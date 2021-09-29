fb7db9989ba8400cc8c7266467bba8dc91997033
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-09-29 15:57:56 +0300

    Added bash script to send message

---
+++ scripts/greengrassV2/sendMessage.sh
@@ -0,0 +1,9 @@
+python3 sendMessage.py \
+  --thing-name THING_NAME \
+  --topic TOPIC \
+  --file FILE \
+  --root-ca root.ca.pem \
+  --cert device.pem.crt \
+  --key private.pem.key \
+  --region REGION \
+  --verbosity Warn
